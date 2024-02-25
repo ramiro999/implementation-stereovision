369cd5b138ac3fac51fe5468b52d6290989d3c01
ramiro999 <ramiro2191920@correo.uis.edu.co>
2024-02-24 19:54:11 -0500

    changes calibration-3

--- self-implementation/calibration_images.py
+++ self-implementation/calibration_images.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 # Initialize video capture for both cameras
 cap = cv2.VideoCapture(0)
-cap2 = cv2.VideoCapture(2)
+cap2 = cv2.VideoCapture(1)
 
 num = 0
 
@@ -34,10 +34,10 @@ while cap.isOpened() and cap2.isOpened():
         plt.show(block=False)
         plt.pause(0.001)  # Pause to ensure the figure is displayed correctly
         
-    k = cv2.waitKey(5)
-    if k == 27:  # ESC key to exitopen
-        break
-    elif k == ord('s'):  # 's' key to save and exit
+        k = cv2.waitKey(5)
+    # if k == 27:  # ESC key to exitopen
+        # break
+    # elif k == ord('s'):  # 's' key to save and exit
         cv2.imwrite('images/stereoLeft/imageL' + str(num) + '.png', img)
         cv2.imwrite('images/stereoRight/imageR' + str(num) + '.png', img2)
         print("Images saved!")
